a37ef66108df66811e60cd02cd3d816c03aa2a67
Adei Josol <helena.josol@gmail.com>
2018-12-18 00:36:34 +0100

    Move transform tests up to reflect execution order

--- app/test_app.py
+++ app/test_app.py
@@ -82,6 +82,23 @@ def test_syllabilise_whitespace_diphthong_ts():
     assert syllabilise('tst s') == ['ts', 't', ' ', 's']
 
 
+def test_transform():
+    assert transform(['b']) == ['b/']
+    assert transform(['b', ' ']) == ['b/', ' ']
+    assert transform(['b', 'b']) == ['b=', 'b/']
+
+
+def test_transform_word_doubling():
+    assert transform(['ba', 'ka'] * 2) == ['ba', 'ka', '\\']
+
+
+def test_transform_syllable_doubling():
+    assert transform(['a', 'a']) == ['a:']
+    assert transform(['ba', 'ba']) == ['ba:']
+    assert transform(['bi', 'bi']) == ['bi1']
+    assert transform(['bi', 'be']) == ['be5']
+
+
 def test_parse_syllable():
     assert parse_syllable(' ') == ('', '', '')
     assert parse_syllable('a') == ('', 'a', '')
@@ -108,20 +125,3 @@ def test_parse_syllable_diphthong_modifier():
     assert parse_syllable('ts:') == ('ts', '', ':')
     assert parse_syllable('nga:') == ('ng', 'a', ':')
     assert parse_syllable('tsa:') == ('ts', 'a', ':')
-
-
-def test_transform():
-    assert transform(['b']) == ['b/']
-    assert transform(['b', ' ']) == ['b/', ' ']
-    assert transform(['b', 'b']) == ['b=', 'b/']
-
-
-def test_transform_word_doubling():
-    assert transform(['ba', 'ka'] * 2) == ['ba', 'ka', '\\']
-
-
-def test_transform_syllable_doubling():
-    assert transform(['a', 'a']) == ['a:']
-    assert transform(['ba', 'ba']) == ['ba:']
-    assert transform(['bi', 'bi']) == ['bi1']
-    assert transform(['bi', 'be']) == ['be5']
