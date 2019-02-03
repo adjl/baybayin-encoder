d2ea41dcb6afeb7ba7704bb2feb0819ea12b8a75
Adei Josol <helena.josol@gmail.com>
2019-02-03 23:44:56 +0000

    Rearrange blocks to a more sensible grouping

--- app/char.py
+++ app/char.py
@@ -3,6 +3,42 @@ import string
 from collections import defaultdict
 
 
+chars = {
+    'whitespace': string.whitespace,
+    'punctuation': string.punctuation,
+    'digits': string.digits,
+    'vowel': 'aeiou',
+    'diphthong': ['ng', 'ts']}
+
+chars['non-consonant'] = ''.join(
+    [chars[char_type] for char_type in ['whitespace', 'punctuation', 'digits', 'vowel']])
+
+
+def get_char_type(char, next_char):
+    for char_type in ['whitespace', 'punctuation', 'vowel']:
+        if char in chars[char_type]:
+            return char_type
+    if concat(char, next_char) in chars['diphthong']:
+        return 'diphthong'
+    return 'consonant'
+
+
+def concat(char, next_char):
+    if next_char is None:
+        return char
+    return ''.join([char, next_char])
+
+
+def parse_syllable(syllable):
+    def get_index(i, func):
+        while i < len(syllable) and func(syllable[i]):
+            i += 1
+        return i
+    cons_i = get_index(0, lambda char: char not in chars['non-consonant'])
+    vowl_i = get_index(cons_i, lambda char: char in chars['vowel'])
+    return syllable[:cons_i], syllable[cons_i:vowl_i], syllable[vowl_i:]
+
+
 class Syllable:
 
     def __init__(self, syllable):
@@ -55,44 +91,9 @@ class SymbolMap(defaultdict):
         return key
 
 
-def parse_syllable(syllable):
-    def get_index(i, func):
-        while i < len(syllable) and func(syllable[i]):
-            i += 1
-        return i
-    cons_i = get_index(0, lambda char: char not in chars['non-consonant'])
-    vowl_i = get_index(cons_i, lambda char: char in chars['vowel'])
-    return syllable[:cons_i], syllable[cons_i:vowl_i], syllable[vowl_i:]
-
-
-def concat(char, next_char):
-    if next_char is None:
-        return char
-    return ''.join([char, next_char])
-
-
-def get_char_type(char, next_char):
-    for name in ['whitespace', 'punctuation', 'vowel']:
-        if char in chars[name]:
-            return name
-    if concat(char, next_char) in chars['diphthong']:
-        return 'diphthong'
-    return 'consonant'
-
-
 symbol_map = SymbolMap()
 symbol_map.update({'j': 'D', 'ñ': '~', 'ng': 'N', 'ts': 'C'})
 symbol_map.update({'aa': ':', 'ii': '1', 'ee': '2', 'uu': '3', 'oo': '4'})
 symbol_map.update({'ie': '5', 'ei': '6', 'uo': '7', 'ou': '8'})
 symbol_map.update({'trailing_consonant': '/', 'non_trailing_consonant': '='})
 symbol_map.update({'word_doubling': '\\', 'consonant_stop': '-'})
-
-
-chars = {
-    'whitespace': string.whitespace,
-    'punctuation': string.punctuation,
-    'digits': string.digits,
-    'vowel': 'aeiou',
-    'diphthong': ['ng', 'ts']}
-chars['non-consonant'] = ''.join(
-    [chars[name] for name in ['whitespace', 'punctuation', 'digits', 'vowel']])
