18880b8c5dda61e96ae10a638898bccb5a862294
Adei Josol <helena.josol@gmail.com>
2019-02-12 01:57:31 +0000

    Redundant test file

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,6 +1,40 @@
-# from app.app import translate
-# from app.char import Syllable
+# from app.syllable import Syllable
 
 
 # def test_transcribe():
-#     assert Syllable(' ').transcribe() == ' '
+# assert Syllable(' ').transcribe() == ' '
+
+
+# def test_transliterate():
+#     assert transliterate(['a']) == ['A']
+#     assert transliterate(['ba']) == ['b']
+#     assert transliterate(['be']) == ['be']
+#     assert transliterate(['b']) == ['b/']
+
+
+# def test_transliterate_shorthand_j():
+#     assert transliterate(['ja']) == ['D']
+#     assert transliterate(['je']) == ['De']
+#     assert transliterate(['j']) == ['D/']
+
+
+# def test_transliterate_shorthand_ñ():
+#     assert transliterate(['ña']) == ['~']
+#     assert transliterate(['ñe']) == ['~e']
+#     assert transliterate(['ñ']) == ['~/']
+
+
+# def test_transliterate_shorthand_ng():
+#     assert transliterate(['nga']) == ['N']
+#     assert transliterate(['nge']) == ['Ne']
+#     assert transliterate(['ng']) == ['N/']
+
+
+# def test_transliterate_shorthand_ts():
+#     assert transliterate(['tsa']) == ['C']
+#     assert transliterate(['tse']) == ['Ce']
+#     assert transliterate(['ts']) == ['C/']
+
+
+# def test_transliterate_multiple_syllables():
+#     assert transliterate(['a', 'ba']) == ['A', 'b']
